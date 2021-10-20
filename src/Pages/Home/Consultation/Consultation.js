cdf5362ff225bff9eccf8bd84d95db2c711ca693
yanisahmed <yanisahmed12@gmail.com>
2021-10-20 18:04:35 +0600

    About Us Gradient effect

--- src/Pages/Home/Consultation/Consultation.js
+++ src/Pages/Home/Consultation/Consultation.js
@@ -13,7 +13,7 @@ const Consultation = () => {
                     <p className="fs-1 fw-bold text-white">Get Online Consultation</p>
                     <p className="text-white">Behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 
-                    <Link to="/appointment"><Button variant="info" className="px-4 border-0 mb-4">Start Video Chat <FontAwesomeIcon icon={faVideo} className="ms-2 text-white" /></Button></Link>
+                    <Link to="/appointment"><Button variant="info" className="px-4 border-0 my-4">Start Video Chat <FontAwesomeIcon icon={faVideo} className="ms-2 text-white" /></Button></Link>
                 </div>
             </div>
         </div>
